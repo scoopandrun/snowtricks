8553bc65187e862bd4acd6a3009e99095ba81bea
Nicolas DENIS <me@scoopandrun.ovh>
2024-02-27 16:20:36 +0100

    Add flash info alerts

---
+++ src/Core/FlashClasses.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Core;
+
+class FlashClasses
+{
+    const PRIMARY = "primary";
+    const SECONDARY = "secondary";
+    const LIGHT = "light";
+    const SUCCESS = "success";
+    const WARNING = "warning";
+    const DANGER = "danger";
+    const INFO = "info";
+}
