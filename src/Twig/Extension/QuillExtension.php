dde5dde5f3cc322a9d47abb6c3284ce7a16f5f7e
Nicolas DENIS <me@scoopandrun.ovh>
2024-03-14 17:55:24 +0100

    Add Quill editor

---
+++ src/Twig/Extension/QuillExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Twig\Extension;
+
+use App\Twig\Runtime\QuillExtensionRuntime;
+use Twig\TwigFilter;
+use Twig\Extension\AbstractExtension;
+
+class QuillExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
+            new TwigFilter(
+                'quill',
+                [QuillExtensionRuntime::class, 'quillFilter'],
+                ['is_safe' => ['html']]
+            ),
+        ];
+    }
+}
