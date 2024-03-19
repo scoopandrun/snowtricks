85c7ba098554c9ffc77df5abc4e9edc8146237bf
Nicolas DENIS <me@scoopandrun.ovh>
2024-03-19 15:18:27 +0100

    Fix SymfonyInsight checks

--- src/Validator/OembedValidator.php
+++ src/Validator/OembedValidator.php
@@ -8,7 +8,7 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class OembedValidator extends ConstraintValidator
 {
-    public function validate($value, Constraint $constraint): void
+    public function validate(mixed $value, Constraint $constraint): void
     {
         /** @var Oembed $constraint */
 
