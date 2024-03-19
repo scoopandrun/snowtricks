29efe2d6a63f8b8c92ea5f39f1084ad435b7846c
Nicolas DENIS <me@scoopandrun.ovh>
2024-03-19 14:59:51 +0100

    Add media overlay

---
+++ assets/controllers/trick-media_controller.js
@@ -0,0 +1,96 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["media"];
+
+  initialize() {
+    this.index = 0;
+  }
+
+  connect() {
+    this.mediaTargets.forEach((element, index) => {
+      element.addEventListener("click", (e) => {
+        this.index = index;
+        this.show();
+      });
+    });
+  }
+
+  get overlay() {
+    /** @type {HTMLDivElement} */
+    const overlay = document.getElementById("media-overlay");
+
+    return overlay;
+  }
+
+  /**
+   * @param {MouseEvent} e
+   */
+  close(e) {
+    const overlay = this.overlay;
+
+    overlay.classList.remove("media-overlay--visible");
+    overlay.classList.add("media-overlay--hidden");
+
+    /** @type {HTMLDivElement} */
+    const captionDiv = overlay.querySelector(".media-overlay__caption");
+    captionDiv.textContent = "";
+
+    /** @type {HTMLDivElement} */
+    const contentDiv = overlay.querySelector(".media-overlay__content");
+    contentDiv.innerHTML = null;
+  }
+
+  show() {
+    /** @type {HTMLButtonElement} */
+    const button = this.mediaTargets[this.index];
+
+    /** @type {"picture"} */
+    const mediaType = button.dataset.type;
+    const caption = button.dataset.caption;
+    const src = button.firstElementChild.src;
+    const content = this.makeContent(mediaType, src);
+
+    const overlay = this.overlay;
+    overlay.classList.remove("media-overlay--hidden");
+    overlay.classList.add("media-overlay--visible");
+
+    /** @type {HTMLDivElement} */
+    const captionDiv = overlay.querySelector(".media-overlay__caption");
+    captionDiv.textContent = caption;
+
+    /** @type {HTMLDivElement} */
+    const contentDiv = overlay.querySelector(".media-overlay__content");
+    contentDiv.innerHTML = null;
+    contentDiv.appendChild(content);
+  }
+
+  prev() {
+    this.index =
+      this.index - 1 >= 0 ? this.index - 1 : this.mediaTargets.length - 1;
+    this.show();
+  }
+
+  next() {
+    this.index = this.index + 1 < this.mediaTargets.length ? this.index + 1 : 0;
+    this.show();
+  }
+
+  /**
+   * @param {"picture"} type
+   * @param {string} src
+   */
+  makeContent(type, src) {
+    if (type === "picture") {
+      const img = document.createElement("img");
+      img.src = src;
+
+      const container = document.createElement("div");
+      container.classList.add("media-overlay__container");
+
+      container.appendChild(img);
+
+      return container;
+    }
+    }
+  }
