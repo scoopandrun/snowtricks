6709848bdff07987c69537e69598c2a9dc6cbc7f
Nicolas DENIS <me@scoopandrun.ovh>
2024-03-26 17:54:23 +0100

    Add Javascript controllers to app.js
    Also, add noscript button on the homepage.

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,11 +1,17 @@
 import { startStimulusApp } from "@symfony/stimulus-bundle";
 
-import HomepageTricks from "./controllers/homepage-tricks_controller.js";
+import Homepage from "./controllers/homepage_controller.js";
+import User from "./controllers/user_controller.js";
+import TrickMedia from "./controllers/trick-media_controller.js";
+import CollectionEdit from "./controllers/collection-edit_controller.js";
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
-app.register("homepage-tricks", HomepageTricks);
+app.register("homepage", Homepage);
+app.register("user", User);
+app.register("trick-media", TrickMedia);
+app.register("collection-edit", CollectionEdit);
 
 document.addEventListener("turbo:load", () => addDeleteConfirmations());
 document.addEventListener("turbo:frame-load", () => addDeleteConfirmations());
@@ -13,12 +19,12 @@ document.addEventListener("turbo:frame-load", () => addDeleteConfirmations());
 function addDeleteConfirmations() {
   document.querySelectorAll("[data-action='delete']").forEach((button) => {
     // Remove before adding to avoid duplicate listeners
-    button.removeEventListener("click", addDeleteConfirmation);
-    button.addEventListener("click", addDeleteConfirmation);
+    button.removeEventListener("click", confirmDelete);
+    button.addEventListener("click", confirmDelete);
   });
 }
 
-function addDeleteConfirmation(e) {
+function confirmDelete(e) {
   const deleteConfirmed = confirm("Are you sure that you want to delete this?");
 
   if (!deleteConfirmed) {
--- assets/controllers/homepage_controller.js
+++ assets/controllers/homepage_controller.js
@@ -3,6 +3,13 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["grid", "fetchButton"];
 
+  connect() {
+    /** @type {HTMLButtonElement} */
+    const fetchButton = this.fetchButtonTarget;
+
+    fetchButton.removeAttribute("hidden");
+  }
+
   async fetch() {
     /** @type {HTMLButtonElement} */
     const fetchButton = this.fetchButtonTarget;
