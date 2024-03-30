98536c15fa4ac049ee862a029183a2a0ec093437
Nicolas DENIS <me@scoopandrun.ovh>
2024-03-30 17:07:54 +0100

    Add missing import

--- src/Form/UserAccountType.php
+++ src/Form/UserAccountType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\DTO\UserInformationDTO;
 use App\Service\FileManager;
+use App\Validator\Constraints\PasswordRequirements;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
