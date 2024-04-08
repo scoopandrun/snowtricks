1dadb499208f5ff5fc66353cf65b8cbbe4a13652
Nicolas DENIS <me@scoopandrun.ovh>
2024-04-08 15:32:25 +0200

    Add delete button JS controller

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -6,6 +6,7 @@ import TrickMediaController from "./controllers/trick-media_controller.js";
 import CollectionEditController from "./controllers/collection-edit_controller.js";
 import FileController from "./controllers/file_controller.js";
 import FormController from "./controllers/form_controller.js";
+import DeleteButtonController from "./controllers/delete-button_controller.js";
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
@@ -16,22 +17,4 @@ app.register("trick-media", TrickMediaController);
 app.register("collection-edit", CollectionEditController);
 app.register("file", FileController);
 app.register("form", FormController);
-
-document.addEventListener("turbo:load", () => addDeleteConfirmations());
-document.addEventListener("turbo:frame-load", () => addDeleteConfirmations());
-
-function addDeleteConfirmations() {
-  document.querySelectorAll("[data-action='delete']").forEach((button) => {
-    // Remove before adding to avoid duplicate listeners
-    button.removeEventListener("click", confirmDelete);
-    button.addEventListener("click", confirmDelete);
-  });
-}
-
-function confirmDelete(e) {
-  const deleteConfirmed = confirm("Are you sure that you want to delete this?");
-
-  if (!deleteConfirmed) {
-    e.preventDefault();
-  }
-}
+app.register("delete-button", DeleteButtonController);
